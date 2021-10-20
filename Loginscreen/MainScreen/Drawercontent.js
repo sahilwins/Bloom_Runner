705dcdc48fa81c3ef8469fe2ca3c364d6c5db208
sahilwins <sahilshwins@gmail.com>
2021-10-20 18:58:17 +0530

    m

--- Loginscreen/MainScreen/Drawercontent.js
+++ Loginscreen/MainScreen/Drawercontent.js
@@ -1,48 +1,88 @@
-import React from 'react'
-import { StyleSheet, View,SafeAreaView} from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler';
-import{
-    Avatar,
-    Title,
-    caption,
-    Paragraph,
-    Drawer,
-    Text,
-    TouchableRipple,
-    Switch
+import React from 'react';
+import {StyleSheet, View, SafeAreaView} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+import {
+  Avatar,
+  Title,
+  Caption,
+  Paragraph,
+  Drawer,
+  Text,
+  TouchableRipple,
+  Switch,
 } from 'react-native-paper';
+import {DrawerItem} from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import LoginFF from './LoginFF';
 
-const Drawercontent = (props) => {
-    return (
-        <SafeAreaView style={{flex:1}}>
-            <ScrollView>
-
-            <View style={styles.drawercontent}>
-            <View style={styles.userinfo}>
-                <View>
-                    <Avatar.Image
-                    source={require('../../assets/logo.png')}
-                        
-                    
-                    />
-
-                </View>
+const Drawercontent = ({navigation}) => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.drawercontent}>
+        <View style={styles.userinfo}>
+          <View style={{flexDirection: 'row', marginTop: 15}}>
+            <Avatar.Image source={require('../../assets/logo.png')} />
+            <View style={{marginLeft: 15}}>
+              <Title>Kevin sharma</Title>
+              <Caption> @Indian</Caption>
             </View>
-            </View>
+          </View>
+        </View>
+      </View>
 
-            </ScrollView>
-            </SafeAreaView>
-    )
-}
+      <ScrollView>
+        <Drawer.Section style={styles.drawersect}>
+          <DrawerItem
+            icon={({color, size}) => (
+              <Icon name="home" color={color} size={size} />
+            )}
+            label="Home"
+            onPress={() => {}}
+          />
 
-export default Drawercontent
+          <DrawerItem
+            icon={({color, size}) => (
+              <Icon name="home" color={color} size={size} />
+            )}
+            label="Home"
+            onPress={() => {}}
+          />
+
+          <DrawerItem
+            icon={({color, size}) => (
+              <Icon name="home" color={color} size={size} />
+            )}
+            label="Home"
+            onPress={() => {
+              navigation.navigate('Profile');
+            }}
+          />
+          <DrawerItem
+            icon={({color, size}) => (
+              <Icon name="home" color={color} size={size} />
+            )}
+            label="LogOut"
+            onPress={() => {
+              navigation.navigate('home');
+            }}
+          />
+        </Drawer.Section>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Drawercontent;
 
 const styles = StyleSheet.create({
-drawercontent:{
-    flex:1,
-},
-userinfo:{
-    paddingLeft:20,
-}
-
-})
+  drawercontent: {
+    flex: 1,
+  },
+  userinfo: {
+    paddingLeft: 20,
+  },
+  drawersect: {
+    // marginTop:1
+    marginBottom: 195,
+  },
+});
--- Loginscreen/TabsScreen/Profilescreen.js
+++ Loginscreen/TabsScreen/Profilescreen.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
 
 const Profilescreen = () => {
+  const Data = [
+    {
+      name: 'hello',
+    },
+  ];
   return (
     <View style={{flex: 1, justifyContent: 'center'}}>
       <Text style={{textAlign: 'center', fontSize: 45}}>Profile Screen</Text>
+
+      <FlatList
+        data={Data}
+        renderItem={({item}) => {
+          return (
+            <View>
+              <Text>{item.name}</Text>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 };
